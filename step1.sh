79242ce094bee529fd52e8a91bf7062cf29a84dd
Unknown <aam14@rice.edu>
2017-04-09 18:07:00 -0500

    Small bug fix

--- step1.sh
+++ step1.sh
@@ -9,7 +9,7 @@ sudo apt-get upgrade -y
 sudo apt-get install -y build-essential python3-pip python3-dev git python3-numpy swig python3-dev
 
 # Blacklist Noveau which has some kind of conflict with the nvidia driver
-echo -e "blacklist nouveau\nblacklist lbm-nouveau\noptions nouveau modeset=0\nalias nouveau off\nalias lbm-nouveau off\n" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf
+echo "blacklist nouveau\nblacklist lbm-nouveau\noptions nouveau modeset=0\nalias nouveau off\nalias lbm-nouveau off\n" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf
 echo options nouveau modeset=0 | sudo tee -a /etc/modprobe.d/nouveau-kms.conf
 sudo update-initramfs -u
 sudo reboot # Reboot (annoying you have to do this in 2015!)
