3157e2306376eec18f1fcce8d4fde140350426d3
abhmul <aam14@rice.edu>
2017-08-28 11:30:45 -0500

    Update step3.sh
    Added installation of PyTorch and PyJet

--- step3.sh
+++ step3.sh
@@ -45,6 +45,12 @@ sudo pip3 install tensorflow-gpu==1.3.0
 sudo pip3 install keras
 sudo pip3 install h5py
 
+# Install PyTorch and PyJet
+sudo pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post2-cp35-cp35m-manylinux1_x86_64.whl
+sudo pip install torchvision
+git clone https://github.com/abhmul/PyJet
+sudo pip install -e .
+
 # Install some other useful packages
 sudo pip3 install pillow
 sudo pip3 install scikit-learn
